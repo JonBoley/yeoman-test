92f239878b971d689f350b47140782f59851f75c
Jon Boley <jdb@jboley.com>
2014-04-06 11:55:12 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Setting up a demo according to:
+http://tylerhenkel.com/creating-apps-with-angular-and-node-using-yeoman/
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('810647App')
+  .controller('MainCtrl', function ($scope, $http) {
+    $http.get('/api/awesomeThings').success(function(awesomeThings) {
+      $scope.awesomeThings = awesomeThings;
+    });
+  });
